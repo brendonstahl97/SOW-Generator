0cb58ad397cf201b2ec55c73fcbfd3c78d5c5d02
Brendon <brendonstahl97@gmail.com>
2023-07-07 15:25:01 -0400

    Initial Commit

---
+++ index.mjs
@@ -0,0 +1,99 @@
+import inquirer from "inquirer";
+import { writeFile } from "fs";
+import generateMarkdown from "./utils/generateMarkdown.mjs";
+
+const testData = {
+  clientCompanyName: "Bank Robbers Inc.",
+  sowNumber: "#001",
+  overviewOfWork: "robbing the big bank undetected",
+  purpose: "obtain the rubies",
+  objectives: "to obtain the satchel of rubies undetected",
+  tasks: "Break into the bank, Collect the rubies, Make tons of money",
+  tasksNotIncluded:
+    "Provide a getaway vehicle, Prevent the alarms from going off",
+  deliverables: "Satchel of rubies",
+  clientBudget: "$500",
+  requiredResources: "Getaway Vehicle, Alarm Jammer",
+  clientSigneeName: "Theodore Bigbux",
+};
+
+const questions = [
+  {
+    type: "input",
+    message: "What is the client company name?",
+    name: "clientCompanyName",
+  },
+  {
+    type: "input",
+    message: "What is the SOW number? (Ex. #001)",
+    name: "sowNumber",
+  },
+  {
+    type: "input",
+    message: "What is the overview of work?",
+    name: "overviewOfWork",
+  },
+  {
+    type: "input",
+    message: "What is the purpose of the project?",
+    name: "purpose",
+  },
+  {
+    type: "input",
+    message: "What are the objectives of the project?",
+    name: "objectives",
+  },
+  {
+    type: "input",
+    message:
+      "What are the tasks that you will complete? (Sperated by a comma and one empty space ', ')",
+    name: "tasks",
+  },
+  {
+    type: "input",
+    message:
+      "What are the tasks that you will not complete? (Sperated by a comma and one empty space ', ')",
+    name: "tasksNotIncluded",
+  },
+  {
+    type: "input",
+    message:
+      "What are the deliverables of the project? (Sperated by a comma and one empty space ', ')",
+    name: "deliverables",
+  },
+  {
+    type: "input",
+    message: "What is the client's total budget?",
+    name: "clientBudget",
+  },
+  {
+    type: "input",
+    message:
+      "What are the resources required from the client? (Sperated by a comma and one empty space ', ')",
+    name: "requiredResources",
+  },
+  {
+    type: "input",
+    message: "What is the client's signee name'?",
+    name: "clientSigneeName",
+  },
+];
+
+const writeToFile = (fileName, data) => {
+  writeFile(fileName, data, (err) => {
+    if (err) throw err;
+  });
+};
+
+const init = async () => {
+  // const data = await inquirer.prompt(questions);
+  const data = testData;
+
+  console.log(data);
+  writeToFile(
+    `output/${data.clientCompanyName}_SOW${data.sowNumber}.md`,
+    generateMarkdown(data)
+  );
+};
+
+init();
---
+++ utils/generateMarkdown.mjs
@@ -0,0 +1,108 @@
+const date = new Date();
+const currentDate = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+
+const generateMarkdown = (data) => {
+  let tasks = "";
+  
+  data.tasksNotIncluded.split(", ").forEach((task) => {
+    tasks += `\n   * ${task.trim()}`;
+  });
+  
+  let tasksNotIncluded = "";
+  
+  data.tasksNotIncluded.split(", ").forEach((task) => {
+    tasksNotIncluded += `\n   * ${task.trim()}`;
+  });
+  
+  let deliverables = "";
+
+  data.deliverables.split(", ").forEach((deliverable) => {
+    deliverables += `\n   * ${deliverable.trim()}`;
+  });
+
+  let resourcesRequired = "";
+
+  data.requiredResources.split(", ").forEach((resource) => {
+    resourcesRequired += `\n   * ${resource.trim()}`;
+  });
+
+  return `
+  # Statement of Work
+  ##### By Brendon Stahl
+  ##### For ${data.clientCompanyName}
+  ##### ${currentDate}
+  ##### SOW ${"#" + data.sowNumber}
+  
+  ***
+
+  ### Introduction
+  Brendon Stahl will perform ${data.overviewOfWork} for ${
+    data.clientCompanyName
+  }.
+
+  ### Purpose and Objectives
+  The purpose of this project is to ${
+    data.purpose
+  }. The project objectives are ${data.objectives}.
+
+  ### Tasks
+  Brendon Stahl will complete the following tasks as part of this project: 
+  ${tasks}
+
+  The following tasks are not included in this project's scope and will not be performed by Brendon Stahl:
+  ${tasksNotIncluded}
+
+  ### Deliverables
+  Brendon Stahl will provide the following deliverables as part of this project:
+  ${deliverables}
+
+  ### Schedule
+  This project will follow the below estimated schedule:
+  | Task/Project Phase | Est. Start Date | Est. End Date | Est. Hours Required |
+  |--------------------|-----------------|---------------|---------------------|
+  |                    |                 |               |                     |
+  |                    |                 |               |                     |
+  |                    |                 |               |                     |
+
+
+  ### Location
+  All work related to this project will occur remotely.
+
+  ### Budget, pricing, and payment schedule
+  ${data.clientCompanyName} has a total budget of ${
+    data.clientBudget
+  } for this project, not to be exceeded.
+  Brendon Stahl will charge the following rates per deliverable:
+  | Deliverable | Price |
+  |-------------|-------|
+  |             |       |
+  |             |       |
+  |             |       |
+
+
+  ### Resources
+  ${
+    data.clientCompanyName
+  } will supply the following resources to enable Brendon Stahl to complete this project:
+  ${resourcesRequired}
+
+  ### Special Items and Conditions
+
+  ### Signatures
+  Signed and agreed to by:
+
+  Brendon Stahl
+
+  Signature _________________________
+  Date ______________________________
+
+
+  ${data.clientCompanyName}
+  ${data.clientSigneeName}
+
+  Signature _________________________
+  Date ______________________________
+  `;
+};
+
+export default generateMarkdown;
